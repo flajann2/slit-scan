63ec04a64d350e5fba26265a6dcfb877cbe95398
Fred Mitchell <fred.mitchell@gmx.de>
2022-04-29 21:34:42 +0200

    getting there

--- src/Slit.hs
+++ src/Slit.hs
@@ -1,6 +1,21 @@
+-- Slit of an image
 module Slit 
-    ( someFunc
+    ( Slit (..)
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- Not all of the following are needed here! FIX
+import Control.Monad
+import Criterion.Main
+import Data.Array.Repa                     as R
+import Data.Array.Repa.Algorithms.Convolve as R
+import Data.Array.Repa.Eval                as R
+import Data.Array.Repa.Repr.Unboxed        as R
+import Data.Array.Repa.Stencil             as R
+import Data.Array.Repa.Stencil.Dim2        as R
+
+import Graphics.Image                      as I
+import Graphics.Image.IO                   as I
+import Graphics.Image.Interface            as I
+import Graphics.Image.Interface.Repa
+import Prelude                             as P
+
