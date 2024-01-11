e0cd8ac55d1da9c70d36089393055bae2f72d06e
El Toro Maestro <eltoro@atomlogik.de>
2024-01-11 12:56:40 -0500

    cleaning up the code

--- app/SlitScan.hs
+++ app/SlitScan.hs
@@ -1,23 +1,6 @@
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
+-- main for SlitScan, cli.
 
 module Main where
-
-import Control.Monad
-import Criterion.Main
-import Data.Array.Repa                     as R
-import Data.Array.Repa.Algorithms.Convolve as R
-import Data.Array.Repa.Eval                as R
-import Data.Array.Repa.Repr.Unboxed        as R
-import Data.Array.Repa.Stencil             as R
-import Data.Array.Repa.Stencil.Dim2        as R
-
-import Graphics.Image                      as I
-import Graphics.Image.IO                   as I
-import Graphics.Image.Interface            as I
-import Graphics.Image.Interface.Repa
-import Prelude                             as P
 
 import CommandLine
 import Scan
